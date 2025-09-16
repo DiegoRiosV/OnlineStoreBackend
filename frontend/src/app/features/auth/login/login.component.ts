248305a2c41649af88087032b63806422662d01d
vdeviv <revollovivian@gmail.com>
2025-09-16 00:52:18 -0330

    Conexion Login Autentificada

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { Client } from '../../../core/models/client.model';
 
 @Component({
   selector: 'app-login',
@@ -35,21 +36,11 @@ export class LoginComponent {
   constructor(private http: HttpClient, private router: Router) {}
 
   onSubmit() {
-    this.http.get<any[]>('http://localhost:8080/clients/all').subscribe({
-      next: (clients) => {
-        const client = clients.find(c => c.mail === this.email && c.password === this.password);
-        if (client) {
-          this.loggedClient = client;
-          this.errorMessage = '';
-          alert(`Bienvenido, ${client.name}`);
-          this.router.navigate(['/']);
-        } else {
-          this.errorMessage = 'Credenciales inválidas';
-        }
-      },
-      error: () => {
-        this.errorMessage = 'Error al conectar con el servidor';
-      }
+    this.http.post<Client>('http://localhost:8080/clients/login',
+      { email: this.email, password: this.password }
+    ).subscribe({
+      next: client => { this.loggedClient = client; this.router.navigate(['/']); },
+      error: _ => this.errorMessage = 'Credenciales inválidas'
     });
   }
 
